ad5910ab1a9b6ea417f4ec31cf84078163255992
od <od@sweettracker.co.kr>
2023-02-06 00:28:41 +0900

    Initial commit

---
+++ src/main/java/com/sewoomprojectv1/SewoomProjectV1Application.java
@@ -0,0 +1,13 @@
+package com.sewoomprojectv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewoomProjectV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SewoomProjectV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sewoomprojectv1/SewoomProjectV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sewoomprojectv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SewoomProjectV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
