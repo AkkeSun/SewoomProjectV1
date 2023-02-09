664a2972fd91f09e8eb1b4bc0336d54a4e3bd974
od <od@sweettracker.co.kr>
2023-02-10 00:04:46 +0900

    feat: ResponseDTO 에 @ToString 추가

--- src/main/java/com/sewoomprojectv1/domain/common/ResponseDTO.java
+++ src/main/java/com/sewoomprojectv1/domain/common/ResponseDTO.java
@@ -2,9 +2,11 @@ package com.sewoomprojectv1.domain.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public class ResponseDTO  {
     private boolean success;
     private Object data;
